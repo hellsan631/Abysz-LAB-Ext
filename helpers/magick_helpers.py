635a4fed4d844e4d4954b95cc963d000f3871763
Mathew Kleppin <matkle414@gmail.com>
2023-11-09 22:01:23 -0500

    Update magick_helpers.py

--- helpers/magick_helpers.py
+++ helpers/magick_helpers.py
@@ -1,7 +1,15 @@
-
+import os
 
 def get_magic_command(command, magick_executible_override_path = None):
     if magick_executible_override_path is None:
         return f'magick {command}'
     
+    # ensure that `magick_executible_override_path` actually exists
+    if not os.path.exists(magick_executible_override_path):
+        raise FileNotFoundError(f'Could not find ImageMagick executable at {magick_executible_override_path}')
+    
+    # ensure that `magick_executible_override_path` is properly escaped for windows
+    # so that the future `subprocess.check_call` does not fail, as the file path might include spaces
+    if os.name == 'nt':
+        magick_executible_override_path = f'"{magick_executible_override_path}"'
     return f'{magick_executible_override_path} {command}'
