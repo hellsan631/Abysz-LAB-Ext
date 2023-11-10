dd2c6a8a8c03d3f690de7258b3f140f4896d936e
Mathew Kleppin <matkle414@gmail.com>
2023-11-09 21:57:49 -0500

    Update system_helpers.py

--- helpers/system_helpers.py
+++ helpers/system_helpers.py
@@ -2,6 +2,7 @@ import subprocess
 
 def run_system_command(command, job_path = None):
     try:
+        print(f"Running command: {command} and {job_path}")
         subprocess.check_call(command, cwd=job_path, shell=True)
     except subprocess.CalledProcessError as e:
         print(f"Error with command: {command} and {job_path}")
